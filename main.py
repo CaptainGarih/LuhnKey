5fc218817cf1707666870d78ebf6fa1914b89951
ShadoIsHere <86681208+ShadoIsHere@users.noreply.github.com>
2023-09-16 21:23:33 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,27 @@
+def luhn_key(card_number):
+    reversed_digits = card_number[::-1]
+
+    doubled_digits = [int(reversed_digits[i]) * 2 if i % 2 == 1 else int(reversed_digits[i]) for i in range(len(reversed_digits))]
+
+    subtracted_digits = [x - 9 if x > 9 else x for x in doubled_digits]
+
+    total = sum(subtracted_digits)
+
+    if total % 10 == 0:
+        key = 0
+    else:
+        key = 10 - (total % 10)
+
+    return key
+
+# Exemple d'utilisation
+state = False
+while state == False:
+    cmd = input("> ")
+    if "stop" in cmd:
+        state = True
+    else:
+        carte_partielle = cmd
+        if len(carte_partielle) == 15:
+            cle_luhn = luhn_key(carte_partielle)
+            print("Le 16ème chiffre de la carte est :" + cle_luhn)
